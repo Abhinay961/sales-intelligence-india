f1abfe87f4b3de15efda83c6f763bc700980feb8
Abhinay Mishra <abhinaymishra@Abhinays-MacBook-Air.local>
2026-04-02 19:04:19 +0530

    Fix: added profit column

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,4 +10,10 @@ def run_preprocessing():
     df["order_date"] = pd.to_datetime(df["order_date"])
     df["month"] = df["order_date"].dt.month
 
+    # -------------------------------
+    # ADD PROFIT COLUMN (FIX)
+    # -------------------------------
+    df["cost"] = df["price"] * 0.7   # assume 30% margin
+    df["profit"] = (df["price"] - df["cost"]) * df["quantity"]
+
     df.to_csv("data/processed/data.csv", index=False)
